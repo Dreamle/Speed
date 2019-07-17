221ce6c4177ae5aeff446e3762d70e8757207c57
admin <404436209@qq.com>
2019-07-17 14:47:47 +0800

    提交一下

--- A/画圆盘/ViewController.m
+++ A/画圆盘/ViewController.m
@@ -20,10 +20,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    XF_MyCircleView *circleView = [[XF_MyCircleView alloc] initWithFrame:CGRectMake(100, 100, 200, 180)];
+    
+    XF_MyCircleView *circleView = [[XF_MyCircleView alloc] initWithFrame:CGRectMake(0, 100, (375 - 30)/3.0, 80)];
     circleView.segmentColocArr =  @[K_blueColor,K_DeepColor,K_DeepColor];
     circleView.segmentDataArr = @[@"0.00%",@"",@"",@"7.50%",@"",@"",@"15.00%"];;
-    
+    circleView.backgroundColor = [UIColor orangeColor];
     [circleView bulidUI];
     self.circleView = circleView;
     [self.view addSubview:circleView];
@@ -34,12 +35,17 @@
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     
-    NSTimer *time = [NSTimer scheduledTimerWithTimeInterval:.1 target:self selector:@selector(k_time) userInfo:nil repeats:YES];
+//    NSTimer *time = [NSTimer scheduledTimerWithTimeInterval:.1 target:self selector:@selector(k_time) userInfo:nil repeats:YES];
+//
+//    [time fire];
+//    self.timer = time;
+//
+//    self.currentFloat = 0.0;
     
-    [time fire];
-    self.timer = time;
-   
-    self.currentFloat = 0.0;
+    self.currentFloat = 0.71;
+    CGFloat allAngle = M_PI + M_PI_4;
+    allAngle = allAngle * (self.currentFloat);
+    self.circleView.lineView.transform = CGAffineTransformRotate(self.circleView.lineView.transform, allAngle);
 }
 
 - (void)k_time {
